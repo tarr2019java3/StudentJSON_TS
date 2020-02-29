eded6ec2625722ce193734d47233ff179b1a0b07
SYTEKTOMASZ <59762629+SYTEKTOMASZ@users.noreply.github.com>
2020-02-29 11:38:49 +0100

    zapis obiektu do json

--- src/pl/sda/student/Main.java
+++ src/pl/sda/student/Main.java
@@ -1,8 +1,22 @@
 package pl.sda.student;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	Student student = new Student("Jan", "Kowalski", 32);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            objectMapper.writeValue(new File("student.json"), student);
+        }
+        catch (IOException e)   {
+            e.printStackTrace();
+        }
     }
 }
