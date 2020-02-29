817c22cf1be5144d99bcad279e190a9183864ce1
SYTEKTOMASZ <59762629+SYTEKTOMASZ@users.noreply.github.com>
2020-02-29 11:45:37 +0100

    stworzenie 5 studentów

--- src/pl/sda/student/Main.java
+++ src/pl/sda/student/Main.java
@@ -9,14 +9,28 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) {
-	Student student = new Student("Jan", "Kowalski", 32);
+
+        Student student = new Student("Jan", "Kowalski", 32);
+        Student student1 = new Student("Jan", "Kowalski", 32);
+        Student student2 = new Student("Michał", "Kowalski", 32);
+        Student student3 = new Student("Marek", "Kowalski", 32);
+        Student student4 = new Student("Maciej", "Kowalski", 32);
+        Student student5 = new Student("Tomasz", "Kowalski", 32);
 
         ObjectMapper objectMapper = new ObjectMapper();
         try {
             objectMapper.writeValue(new File("student.json"), student);
+            objectMapper.writeValue(new File("student1.json"), student1);
+            objectMapper.writeValue(new File("student2.json"), student2);
+            objectMapper.writeValue(new File("student3.json"), student3);
+            objectMapper.writeValue(new File("student4.json"), student4);
+            objectMapper.writeValue(new File("student5.json"), student5);
+
         }
         catch (IOException e)   {
             e.printStackTrace();
         }
+
+
     }
 }
